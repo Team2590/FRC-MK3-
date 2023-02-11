b654cc23e075345453998460c7639cd951c1ba0c
AYAN KAWLEKAR <92340487+AYANKAWLEKAR@users.noreply.github.com>
2023-02-11 14:29:34 -0500

    b

--- src/main/java/frc/auto/trajectory/NemesisPath.java
+++ src/main/java/frc/auto/trajectory/NemesisPath.java
@@ -4,45 +4,66 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.math.trajectory.Trajectory.State;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.subsystems.Drivetrain;
+
+import com.pathplanner.lib.PathPlannerTrajectory.EventMarker;
+import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
+
 
 public class NemesisPath {
     private final Timer timer = new Timer();
     private boolean isStarted;
     private boolean isFinished;
+    private List<PathPlannerTrajectory> pathGroup;
     private Trajectory path;
+    private PathPlannerTrajectory ontheFly;
+    
     public NemesisPath(TrajectoryConfig config, Pose2d initialPose, Pose2d finalPose2d, Translation2d[] waypoints){
         isFinished = false;
         isStarted = false;
         path = TrajectoryGenerator.generateTrajectory(initialPose, List.of(waypoints), finalPose2d, config); 
     }
-    public NemesisPath(Path pathJSON){       
+    public NemesisPath(String pathJSON, double maxVelocity, double maxAcceleration){       
         isFinished = false;
         isStarted = false;
-        try {
-            path = TrajectoryUtil.fromPathweaverJson(pathJSON);
+        
+            pathGroup = PathPlanner.loadPathGroup(pathJSON, new PathConstraints(maxVelocity, maxAcceleration));
             System.out.println("PATH INITIALIZED");
-        } catch (IOException ex) {
-            DriverStation.reportError("Path not Found" , ex.getStackTrace());
-        }
+        
+     
     }
-    public void runPath(Drivetrain drivetrain){
+    public NemesisPath(PathPoint initial,PathPoint ending, double maxVelocity, double maxAcceleration){
+        isFinished=false;
+        isStarted=false;
+        ontheFly=PathPlanner.generatePath(new PathConstraints(maxVelocity, maxAcceleration), initial, ending);
+        pathGroup.add(0,ontheFly);
+    }//on the fly 
+    public void runPath(Drivetrain drivetrain, int pathNum){
         if(!isStarted){
         // start it
             System.out.println("STARTING PATH"+"reset encoders");
+            
+            SmartDashboard.putNumber("path_parts", pathGroup.size());
             drivetrain.resetEncoder();
-            drivetrain.resetOdometry(path.getInitialPose(), drivetrain.getSwervePositions());
+            drivetrain.resetOdometry(pathGroup.get(0).getInitialHolonomicPose(), drivetrain.getSwervePositions());
             timer.reset();
-            timer.start();
+            timer.start(); 
             isStarted = true;
         } 
         else if(isStarted) {
@@ -51,15 +72,30 @@ public class NemesisPath {
             SmartDashboard.putNumber("Time:", currentTime);
             SmartDashboard.putNumber("Sample X:", path.sample(currentTime).poseMeters.getX());
             SmartDashboard.putNumber("Sample Y:", path.sample(currentTime).poseMeters.getY());
-            drivetrain.followPath(path.sample(currentTime));   
+            drivetrain.followPath(pathGroup.get(pathNum).sample(currentTime));
+                
+
+                
+            
+            
+             
         }
     }
     public boolean getIsStarted(){
         return isStarted;
     }
-    public boolean getIsFinished(){
-        isFinished = timer.get() > path.getTotalTimeSeconds();
+    public boolean getIsFinished(int pathNum){
+        isFinished = timer.get() > pathGroup.get(pathNum).getTotalTimeSeconds();
         return isFinished;
+    }
+    public double getTime(){
+        return timer.get();
+    }
+    public List<EventMarker> getEventMarkers(int pathNum){
+        return pathGroup.get(pathNum).getMarkers();
+    }
+    public State getEventState(int time, int pathNum){
+        return pathGroup.get(pathNum).getStates().get(time);
     }
     public void setWait(long duration){
         try{
